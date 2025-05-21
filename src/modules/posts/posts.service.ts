e2bf5630276688de4e1f7039ac92c10b6b57316a
Enzorcand <enzorcand@gmail.com>
2025-05-21 16:12:15 -0300

    changed the reviewsFromUserMethod to make it return all the info for the front end

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -9,46 +9,60 @@ export class PostsService {
 
   async getRecentReviewsFromUser(userId: number) {
     const reviews = await this.prismaService.post.findMany({
-      select: {
-        book_id: true,
-        user_id: true,
-        is_spoiler: true,
-        title: true,
-        text: true,
-        is_review: true,
-        parent_id: true,
-        created_at: true,
-        livro: {
-          select: {
-            google_image_url: true,
-            title: true,
-          },
-        },
-        user: {
-          select: {
-            username: true,
-          },
-        },
-        _count: {
-          select: {
-            liked_by: true,
-          },
-        },
-      },
       where: {
         user_id: userId,
-        is_review: true,
-      },
-      orderBy: {
-        created_at: 'desc',
-      },
-      take: 5,
+        parent_id: null,
+      } 
+      // select: {
+      //   book_id: true,
+      //   user_id: true,
+      //   is_spoiler: true,
+      //   title: true,
+      //   text: true,
+      //   is_review: true,
+      //   parent_id: true,
+      //   created_at: true,
+      //   livro: {
+      //     select: {
+      //       google_image_url: true,
+      //       title: true,
+      //     },
+      //   },
+      //   user: {
+      //     select: {
+      //       username: true,
+      //     },
+      //   },
+      //   _count: {
+      //     select: {
+      //       liked_by: true,
+      //     },
+      //   },
+      // },
+      // where: {
+      //   user_id: userId,
+      //   is_review: true,
+      // },
+      // orderBy: {
+      //   created_at: 'desc',
+      // },
+      // take: 5,
     })
+    // Fetch children for each review
+    const reviewsWithChildren = await Promise.all(
+      reviews.map(async (post) => {
+      const children = await this.prismaService.post.findMany({
+        where: { parent_id: post.post_id },
+      });
+      return {
+        ...post,
+        children,
+        likes: post._count?.liked_by
+      };
+      })
+    );
 
-    return reviews.map((post) => ({
-      ...post,
-      likes: post._count.liked_by,
-    }))
+    return reviewsWithChildren;
   }
 
   async createNewPost(dto: PostDto, userId: number) {
