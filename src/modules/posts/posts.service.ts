d3e9b1bf95ca50e0a09262b71465b1794e7720ff
Guilherme Moraes <guimps1909@gmail.com>
2025-06-15 02:21:01 -0300

    fix: transforming bytes to string

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
+import { fromByteArray } from "base64-js";
 import { PaggesLogger } from "src/config/winston-logger/pagges-logger.utils";
 import { PrismaService } from "../prisma/prisma.service";
 import { PostDto } from "./dto/post.dto";
-import { fromByteArray } from "base64-js";
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) {}
@@ -11,7 +11,7 @@ export class PostsService {
     const reviews = await this.prismaService.post.findMany({
       where: {
         parent_id: postId,
-      }, 
+      },
       include: {
         user: {
           select: {
@@ -34,7 +34,7 @@ export class PostsService {
           },
         },
       },
-    })
+    });
 
     const response = reviews.map((post) => ({
       ...post,
@@ -112,7 +112,7 @@ export class PostsService {
     const postsByBook = await this.prismaService.post.findMany({
       where: {
         book_id: id,
-        parent_id: null, 
+        parent_id: null,
       },
       include: {
         user: {
@@ -157,57 +157,62 @@ export class PostsService {
     };
   }
 
- async getFollowingPosts(userId: number) {
-  const followingUsers = await this.prismaService.userFollow.findMany({
-    where: { follower_id: userId },
-    select: { following_id: true },
-  });
+  async getFollowingPosts(userId: number) {
+    const followingUsers = await this.prismaService.userFollow.findMany({
+      where: { follower_id: userId },
+      select: { following_id: true },
+    });
 
-  const followingIds = followingUsers.map((follow) => follow.following_id);
+    const followingIds = followingUsers.map((follow) => follow.following_id);
 
-  const posts = await this.prismaService.post.findMany({
-    where: { 
-    user_id: { in: followingIds },
-    is_review: true,
-    },
-    include: {
-      user: {
-        select: {
-          user_id: true,
-          username: true,
-          name: true,
-          profile_image: true,
+    const posts = await this.prismaService.post.findMany({
+      where: {
+        user_id: { in: followingIds },
+        is_review: true,
+      },
+      include: {
+        user: {
+          select: {
+            user_id: true,
+            username: true,
+            name: true,
+            profile_image: true,
+          },
+        },
+        livro: {
+          select: {
+            title: true,
+            google_image_url: true,
+          },
+        },
+        _count: {
+          select: {
+            liked_by: true,
+            comments: true,
+          },
         },
       },
-      livro: {
-        select: {
-          title: true,
-          google_image_url: true,
-        },
-      },
-      _count: {
-        select: {
-          liked_by: true,
-          comments: true,
-        },
-      },
-    },
-    orderBy: { created_at: 'desc' },
-  });
+      orderBy: { created_at: "desc" },
+    });
 
-  return {
-    status: 200,
-    message: 'Posts dos usuários seguidos retornados com sucesso',
-    data: posts.map((post) => ({
-      post_id: post.post_id,
-      autor: post.user,
-      texto: post.text,
-      dataPostagem: post.created_at,
-      curtidas: post._count.liked_by,
-      comentarios: post._count.comments,
-      spoiler: post.is_spoiler,
-      livro: post.livro,
-      id_postPai: post.parent_id,
+    return {
+      status: 200,
+      message: "Posts dos usuários seguidos retornados com sucesso",
+      data: posts.map((post) => ({
+        post_id: post.post_id,
+        autor: {
+          ...post.user,
+          profile_image: post.user.profile_image
+            ? fromByteArray(post.user.profile_image)
+            : null,
+        },
+        texto: post.text,
+        dataPostagem: post.created_at,
+        curtidas: post._count.liked_by,
+        comentarios: post._count.comments,
+        spoiler: post.is_spoiler,
+        livro: post.livro,
+        id_postPai: post.parent_id,
       })),
     };
   }
